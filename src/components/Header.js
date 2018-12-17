a6d2f7838508713f0671a3ba09e4131c51adc019
grace2t <43977340+grace2t@users.noreply.github.com>
2018-12-18 10:32:44 +1100

    Added comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import TodoTextInput from './TodoTextInput'
 
+// this is a stateless functional component (SFC) [fn. 1]
 const Header = ({ addTodo }) => (
   <header className="header">
     <h1>todos</h1>
@@ -22,3 +23,12 @@ Header.propTypes = {
 }
 
 export default Header
+
+/* FOOTNOTES                                                                  ->
+1. Stateless Functional Component https://tylermcginnis.com/functional-components-vs-stateless-functional-components-vs-stateless-components/
+                               https://www.jstwister.com/post/react-stateless-functional-components-best-practices/
+                               https://javascriptplayground.com/functional-stateless-components-react/
+   SFCs are simple. These components are solely props in, rendered UI out.
+   They are not tied to React, other than the fact it uses the JSX syntax.
+
+*/

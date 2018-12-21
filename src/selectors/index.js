7e5dfa03c6c17bbb268f7fa7a03713e9699bd83a
grace2t <43977340+grace2t@users.noreply.github.com>
2018-12-21 11:08:32 +1100

    add comments

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,31 +1,41 @@
-import { createSelector } from 'reselect'
-import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/TodoFilters'
+import { createSelector } from "reselect";
+import {
+  SHOW_ALL,
+  SHOW_COMPLETED,
+  SHOW_ACTIVE
+} from "../constants/TodoFilters";
 
-const getVisibilityFilter = state => state.visibilityFilter
-const getTodos = state => state.todos
+// these are Redux selectors
+// Selectors are functions that take Redux state as an argument and return
+// some data to pass to the component. They can be simple or do more
+// compilcated data transformations like filter a list.
+// Redux state can be thought of like a database and selectors are like
+// SELECT queries to get useful data (e.g. filtered list) from the database.
+
+const getVisibilityFilter = state => state.visibilityFilter;
+const getTodos = state => state.todos;
 
 export const getVisibleTodos = createSelector(
   [getVisibilityFilter, getTodos],
   (visibilityFilter, todos) => {
     switch (visibilityFilter) {
       case SHOW_ALL:
-        return todos
+        return todos;
       case SHOW_COMPLETED:
-        return todos.filter(t => t.completed)
+        return todos.filter(t => t.completed);
       case SHOW_ACTIVE:
-        return todos.filter(t => !t.completed)
+        return todos.filter(t => !t.completed);
       default:
-        throw new Error('Unknown filter: ' + visibilityFilter)
+        throw new Error("Unknown filter: " + visibilityFilter);
     }
   }
-)
+);
 
-export const getCompletedTodoCount = createSelector(
-  [getTodos],
-  todos => (
-    todos.reduce((count, todo) =>
-      todo.completed ? count + 1 : count,
-      0
-    )
-  )
-)
+export const getCompletedTodoCount = createSelector([getTodos], todos =>
+  todos.reduce((count, todo) => (todo.completed ? count + 1 : count), 0)
+);
+
+/* FOOTNOTES                                                                  ->
+1. Redux selectos https://www.saltycrane.com/blog/2017/05/what-are-redux-selectors-why-use-them/
+
+*/
